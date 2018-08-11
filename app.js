5ce3e3b298f73b7ed5edf8d2927bb9c2cd63488b
lukeloresch <luke.loresch@gmail.com>
2018-08-10 22:08:12 -0500

    basic cli with insert and read all

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 const fs = require('fs')
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-// Connection URL
+var Binary = require('mongodb').Binary;
+var db = require('./db.js');
 const url = 'mongodb://localhost:27017';
-// Database Name
 const dbName = 'deployments';
+    
 var argv = require('yargs')
   .usage('Usage: $0 -e [str] -n [str]')
   .help('help').alias('help', 'h')
@@ -14,47 +15,78 @@ var argv = require('yargs')
       alias: 'e',
       description: "<envrionment> environment name",
       requiresArg: true,
-      required: true
+//      required: true
     },
     name: {
       alias: 'n',
       description: "<service name> name of service",
       requiresArg: true,
-      required: true
+      //required: true
     },
     branch: {
       alias: 'b',
       description: "<branch name> name of branch",
       requiresArg: true,
-      required: true
+      //required: true
     },
     commit: {
       alias: 'c',
       description: "<commit id> commit id",
       requiresArg: true,
-      required: true
+      //required: true
     },
     serviceJson: {
       alias: 'j',
       description: "<serivce.jsonfile>",
       requiresArg: true,
-      required: true
+      //required: true
+    },
+    deployment: {
+      alias: 'd',
+       boolean: true
     }
   })
   .argv;
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-	const db = client.db(dbName);
-	recordDeployment(db, function() {
-    client.close();
+
+if (argv.d) {
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+	  const db = client.db(dbName);
+	  recordDeployment(db, function() {
+      client.close();
+    });
   });
-});
+} else if (argv.g) {
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+	  const db = client.db(dbName);
+	  readDeployments(db, function() {
+      client.close();
+    });
+  });
+}
+
+const readDeployments = function(db, callback) {
+
+ const collection = db.collection(argv.e);
+
+  collection.find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    callback(result)
+  });
+}
 
 const recordDeployment = function(db, callback) {
+
   let d = new Date();
+  var data = fs.readFileSync(`${process.cwd()}/etc/service.json`);
+  var insert_data = {};
+  insert_data.file_data = Binary(data);
+ 
   // Get the documents collection
   const collection = db.collection(argv.e);
    let doc = {
@@ -62,25 +94,13 @@ const recordDeployment = function(db, callback) {
     serviceName: argv.name,
     branch: argv.branch,
     commit: argv.commit,
-    serviceJson: argv.serviceJson
+    serviceJson: insert_data
   }
-
+ 
   collection.insertOne(doc, function (err, res) {
     if (err) throw err;
-    console.log("inserted doc", res.ops[0]);
+    console.log("inserted doc");
+    callback(res);
   });
 }   
-    
- /* 
-  collection.insertMany([
-    { date: new Date(Date.now()).toISOString() },
-    { serviceName: argv.name },
-    { branch: argv.branch },
-    { commit: argv.commit },
-    { serviceJson: argv.serviceJson }
-  ], function(err, result) {
-    callback(result);
-  });
 
-  */
-//}
