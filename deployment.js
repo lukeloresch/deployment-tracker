5708de1921bf1fcae78b12053aefec5a681051cf
lukeloresch <luke.loresch@gmail.com>
2018-08-12 15:15:34 -0500

    before removing old files

--- deployment.js
+++ deployment.js
@@ -5,15 +5,21 @@ const { addDeployment, getDeployment } = require('./logic');
 program
   .version('0.0.1')
   .description('Deployment management system');
-
 program
-  .command('addDeployment <environment> <serviceName> <branch> <commit> <image>')
+  .command('addDeployment <environment> <serviceName> <branch> <commit> <image> <serviceJson file>')
   //.alias('d')
   .description('Add a deployment')
-  .action((environment, serviceName, branch, commit, image) => {
-    addContact({});
+  .action((environment, serviceName, branch, commit, image, serviceJson) => {
+    let o = {
+      environment: environment,
+      serviceName: serviceName,
+      branch: branch, 
+      commit: commit,
+      image: image,
+      serviceJson: serviceJson || null
+    }
+    addDeployment(o);
   });
-
 program
   .command('getDeployment <environment> <key> <value>')
   //.alias('g')
--- logic.js
+++ logic.js
@@ -6,58 +6,46 @@ mongoose.connect('mongodb://localhost/deployments');
 
 var connection = mongoose.connection;
 
-// Connect to a single MongoDB instance. The connection string could be that of a remote server
-// We assign the connection instance to a constant to be used later in closing the connection
-//mongoose.connect('mongodb://localhost:27017/deployments');
 const db = mongoose.connection;
-
-// Converts value to lowercase
-
-// Define a contact Schema
 const deploymentSchema = mongoose.Schema({
   servicename: { type: String },
   image: { type: String },
   branch: { type: String },
   commit: { type: String },
   date: { type: Date }
-});//, {collection: 'dev'});
+});
 
 // Define model as an interface with the database
 const Deployment = mongoose.model('Deployment', deploymentSchema);
 
-/**
- * @function  [addContact]
- * @returns {String} Status
- */
 const addDeployment = (deployment) => {
   let d = new Date();
-  deployment.date = d;
-  Deployment.create(deployment, (err) => {
-    assert.equal(null, err);
-    console.info('New deployment added');
-    db.disconnect();
-    connection.close();
-    
+  //deployment.date = d;
+  console.log(deployment);
+  let environment = deployment.environment
+
+  connection.on('error', console.error.bind(console, 'connection error:'));
+  connection.once('open', function () {
+    connection.db.collection(environment, function(err, collection){
+      collection.insert(deployment);
+    });
   });
+connection.close();
 };
 
-/**
- * @function  [getContact]
- * @returns {Json} contacts
- */
 const getDeployment = (environment, key, value) => {
   var exp = new RegExp('^(.*?)auth')
   var query = {};
   query[key] = exp;
  
-connection.on('error', console.error.bind(console, 'connection error:'));
-connection.once('open', function () {
+  connection.on('error', console.error.bind(console, 'connection error:'));
+  connection.once('open', function () {
     connection.db.collection(environment, function(err, collection){
-        collection.find({}).toArray(function(err, data){
-            console.log(data); // it will print your collection data
-        })
+      collection.find({}).toArray(function(err, data){
+        console.log(data); // it will print your collection data
+      })
     });
-});
+  });
 connection.close();
 };
 
